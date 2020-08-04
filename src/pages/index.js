4d7dd49f0ffdc4a10481dec9ee05a332d0f45ddc
HKey <s06139@gmail.com>
2020-08-04 16:30:33 +0900

    Unify date format to YYYY-MM-DD

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,7 +44,7 @@ export const pageQuery = graphql`
             slug
           }
           frontmatter {
-            date(formatString: "MMMM DD, YYYY")
+            date
             title
             tags
           }
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -94,7 +94,7 @@ export const pageQuery = graphql`
       html
       frontmatter {
         title
-        date(formatString: "MMMM DD, YYYY")
+        date
         tags
       }
     }
